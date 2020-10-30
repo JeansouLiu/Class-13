053dd77359ec07418e74b7ea5a25d836b2f63e80
Jeansou <jeansou@jeansou.com>
2020-10-30 20:21:04 +0800

    Delete nomenu.js

--- js/home/nomenu.js
+++
@@ -1,4 +0,0 @@
-document.oncontextmenu = function(){ return false; };
-document.oncopy = function(){ return false; };
-document.oncut = function(){ return false; };
-document.ondragstart = function(){ return false; };

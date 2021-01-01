d050c1ebdf928fba0c5d19b00b42d9a4755d907c
Jeansou <jeansou@jeansou.com>
2021-01-01 13:22:05 +0800

    Update notification.js

--- js/home/notification.js
+++ js/home/notification.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function () {
     });
   }
     if (window.Notification && Notification.permission === "granted") {
-	  var img = 'img/favicon.ico';
+	  var img = 'https://class13.jeansou.com/forum/public/assets/logo-dm1cnzci.png';
       var notification = new Notification('13班记录站', { body: '眼里有事 心中有光', icon: img });
     }
     else if (window.Notification && Notification.permission !== "denied") {
